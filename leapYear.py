0615378149032e7f514ded7359c60c1ea9969e06
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-14 23:54:19 +0530

    Update leapYear.py

--- leapYear.py
+++ leapYear.py
@@ -1,9 +1,7 @@
 # Python code to find the entered year is a leap year or not
 year = int(input("Enter any Year: \n"))
 
-leapYear = year%4
-
-if leapYear == 0 :
+if year%4 == 0 :
     print("Its a Leap Year")
 else:
-    print("Its not a Leap Year")    
+    print("Its not a Leap Year")     
